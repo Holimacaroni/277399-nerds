7063af0f65baa919c1043fac7356d252a255d3fd
Holimacaroni <birdparker8@gmail.com>
2016-11-18 14:11:58 +0300

    Причесан код с помощью HTMLPrettify

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-
 var link = document.querySelector(".contact-button");
 
 var popup = document.querySelector(".modal-content");
@@ -12,17 +11,17 @@ var username = popup.querySelector("[name=username]");
 var email = popup.querySelector("[name=email]");
 
 var storage = localStorage.getItem("username");
-        
-    link.addEventListener("click", function(event) {
-            event.preventDefault();
-            popup.classList.add("modal-content-show");
 
-        if (storage) {
-               username.value = storage;
-               email.focus();
-        } else {
-               username.focus();
-        }
+link.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.add("modal-content-show");
+
+    if (storage) {
+        username.value = storage;
+        email.focus();
+    } else {
+        username.focus();
+    }
 });
 
 close.addEventListener("click", function(event) {
@@ -41,41 +40,40 @@ form.addEventListener("submit", function(event) {
         localStorage.setItem("username", username.value);
     }
 });
-        
+
 window.addEventListener("keydown", function(event) {
     if (event.keyCode === 27) {
         if (popup.classList.contains("modal-content-show")) {
-        popup.classList.remove("modal-content-show");
-        popup.classList.remove("modal-error");
+            popup.classList.remove("modal-content-show");
+            popup.classList.remove("modal-error");
         }
     }
 });
 
-    ymaps.ready(function () {
-var myMap = new ymaps.Map('map', {
-        center: [59.939000, 30.321375],
-        zoom: 17,
-        controls: ['zoomControl']
-    }, {
-        searchControlProvider: 'yandex#search'
-    }),
-    myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {
-        hintContent: 'Собственный значок метки',
-        balloonContent: 'img/map_pin.png'
-    }, {
-        // Опции.
-        // Необходимо указать данный тип макета.
-        iconLayout: 'default#image',
-        // Своё изображение иконки метки.
-        iconImageHref: 'img/map_pin.png',
-        // Размеры метки.
-        iconImageSize: [231, 190],
-        // Смещение левого верхнего угла иконки относительно
-        // её "ножки" (точки привязки).
-        iconImageOffset: [-60, -200]
-    });
+ymaps.ready(function() {
+    var myMap = new ymaps.Map('map', {
+            center: [59.939000, 30.321375],
+            zoom: 17,
+            controls: ['zoomControl']
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'img/map_pin.png'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/map_pin.png',
+            // Размеры метки.
+            iconImageSize: [231, 190],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-60, -200]
+        });
 
-myMap.geoObjects.add(myPlacemark);
-myMap.behaviors.disable('scrollZoom'); 
+    myMap.geoObjects.add(myPlacemark);
+    myMap.behaviors.disable('scrollZoom');
 });
-
